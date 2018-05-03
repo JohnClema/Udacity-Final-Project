33689549197dcd453e317411577fad4e909858ec
JohnClema <john.clema@gmail.com>
2018-05-03 11:18:15 +0800

    Intial commit

---
+++ Vacuum News/Web Services/NASAPictureService.swift
@@ -0,0 +1,34 @@
+//
+//  NASAPictureService.swift
+//  Vacuum News
+//
+//  Created by John Clema on 18/4/18.
+//  Copyright © 2018 John Clema. All rights reserved.
+//
+
+import Foundation
+
+class NASAPictureService: Service {
+  
+    private let baseURL: String
+    
+    init(baseURL: String) {
+        self.baseURL = baseURL
+    }
+    
+    func create() {
+        
+    }
+    
+    func read() {
+        
+    }
+    
+    func update() {
+        
+    }
+    
+    func delete() {
+        
+    }
+}
---
+++ Vacuum News/Web Services/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  Vacuum News
+//
+//  Created by John Clema on 18/4/18.
+//  Copyright © 2018 John Clema. All rights reserved.
+//
+
+import Foundation
+
+protocol Service {
+    func create()
+    func read()
+    func update()
+    func delete()
+}
